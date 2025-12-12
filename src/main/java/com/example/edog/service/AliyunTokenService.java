a0b97ab441770a14a7fd90947b4ea9440949b53d
yangzhiyong3508 <3313708510@qq.com>
2025-12-12 21:11:49 +0800

    把ASR换为阿里云ASR

---
+++ src/main/java/com/example/edog/service/AliyunTokenService.java
@@ -0,0 +1,4 @@
+package com.example.edog.service;
+
+public class AliyunTokenService {
+}
---
+++ src/main/java/com/example/edog/utils/AliyunCredentials.java
@@ -0,0 +1,9 @@
+package com.example.edog.utils;
+
+public class AliyunCredentials {
+    // 你的 AppKey (在阿里云控制台 -> 智能语音交互 -> 项目管理中获取)
+    public static final String APP_KEY = "你的AppKey";
+
+    // 你的 AccessToken (有效期24小时，生产环境建议写定时任务自动刷新)
+    public static final String TOKEN = "你的AccessToken";
+}
